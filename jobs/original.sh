ac8221cfc09ee9cc393185db944e19276d8430f9
dea.gogishvili <dea.gogishvili@gmail.com>
2024-01-22 21:40:21 +0100

    Config files and bash scripts

---
+++ jobs/original.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --job-name=original
+#SBATCH --partition=defq
+#SBATCH --gpus=A30:1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=3
+#SBATCH --time=80:00:00
+#SBATCH --error=original.err
+#SBATCH --output=original.out
+
+export XLA_FLAGS="--xla_gpu_cuda_data_dir=/scistor/informatica/emi232/anaconda3/envs/dl2022/"
+module load shared
+module load 2022
+source activate dl2022
+
+cd ../patchprot
+
+pip install click
+pip install fair-esm
+pip install .
+
+PROT train -c "configs/esm2_original.yml"
+
+Echo "done"
+

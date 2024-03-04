d73cc00db92c034baf6d1d1ac65c691d5990f876
dea.gogishvili <dea.gogishvili@gmail.com>
2024-03-04 13:30:36 +0100

    Updating configs and .sh scripts

--- jobs/esm2.sh
+++ jobs/esm2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#SBATCH --job-name=train_basic_esm2
+#SBATCH --job-name=train_esm2
 #SBATCH --partition=defq
 #SBATCH --gpus=A30:1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=3
 #SBATCH --time=80:00:00
-#SBATCH --error=esm2_basic.err
-#SBATCH --output=esm2_basic.out
+#SBATCH --error=esm2.err
+#SBATCH --output=esm2.out
 
 export XLA_FLAGS="--xla_gpu_cuda_data_dir=/scistor/informatica/emi232/anaconda3/envs/dl2022/"
 module load shared
@@ -20,7 +20,7 @@ pip install click
 pip install fair-esm
 pip install .
 
-PROT train -c "configs/esm2_basic.yml"
+PROT train -c "configs/esm2.yml"
 
 echo "done"
 
